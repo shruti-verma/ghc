95bca84d8670f315aca5fe2591c7fdedec550159
shruti-verma <shrutiv@amazon.com>
2014-11-05 23:03:35 +0530

    SQLLIte changes-Java classes

---
+++ src/com/csfaq/reportit/DatabaseActivity.java
@@ -0,0 +1,44 @@
+package com.csfaq.reportit;
+
+import android.app.Activity;
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.os.Build;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class DatabaseActivity extends Activity {
+
+	TextView idView;
+	EditText category;
+	EditText status;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.ios_frag);
+
+		
+	}
+
+	@Override
+	protected void onStart()
+	{
+		super.onStart();
+		idView = (TextView) findViewById(R.id.textView);
+    		category = (EditText) findViewById(R.id.textView2);
+    			}
+
+	
+        public void newCompliant (View view) {
+    	   MyDBHandler dbHandler = new MyDBHandler(this, null, null, 1,null);
+    	
+      }
+    
+      
+}
---
+++ src/com/csfaq/reportit/MyDBHandler.java
@@ -0,0 +1,48 @@
+package com.csfaq.reportit;
+
+
+import android.content.Context;
+import android.database.DatabaseErrorHandler;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteDatabase.CursorFactory;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class MyDBHandler extends SQLiteOpenHelper {
+
+	private static final int DATABASE_VERSION = 1;
+	private static final String DATABASE_NAME = "ReportIt.db";
+	public static final String TABLE_COMPLIANTS = "Complaints";
+	
+	public static final String COLUMN_ID = "_id";
+	public static final String COLUMN_COMPLIANTNUM = "compliantnum";
+	public static final String COLUMN_CATEGORY = "catgeory";
+	public static final String COLUMN_STATUS = "status";
+	public static final String COLUMN_USERNAME = "username";
+	public static final String COLUMN_COMMENTS = "comments";
+	
+	public MyDBHandler(Context context, String name, CursorFactory factory,
+			int version, DatabaseErrorHandler errorHandler) {
+		super(context, DATABASE_NAME, null, DATABASE_VERSION);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void onCreate(SQLiteDatabase db) {
+
+		String CREATE_COMPLIANTS_TABLE = "CREATE TABLE " +
+	             TABLE_COMPLIANTS + "("
+	             + COLUMN_ID + " INTEGER PRIMARY KEY," + COLUMN_COMPLIANTNUM 
+	             + " INTEGER," + COLUMN_CATEGORY+ " STRING," + COLUMN_STATUS+ " STRING," + COLUMN_USERNAME+ " STRING," 
+	             + COLUMN_COMMENTS+ " STRING" + ")";
+		
+	      db.execSQL(CREATE_COMPLIANTS_TABLE);
+	}
+
+	@Override
+	public void onUpgrade(SQLiteDatabase db, int arg1, int arg2) {
+		// TODO Auto-generated method stub
+		db.execSQL("DROP TABLE IF EXISTS " + TABLE_COMPLIANTS);
+	      onCreate(db);
+	}
+}
+
