453dd5cb6f0748d0a47cce30e8b5f4eb1f989e0f
shruti-verma <shrutiv@amazon.com>
2014-11-05 23:03:07 +0530

    SQLLite DB changes

--- gen/com/csfaq/reportit/R.java
+++ gen/com/csfaq/reportit/R.java
@@ -8,6 +8,9 @@
 package com.csfaq.reportit;
 
 public final class R {
+    public static final class array {
+        public static final int complaint_category=0x7f060000;
+    }
     public static final class attr {
     }
     public static final class dimen {
@@ -25,18 +28,24 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int Cancel=0x7f08000a;
-        public static final int Category=0x7f080001;
-        public static final int CategoryList=0x7f080002;
-        public static final int ImageVideoPicker=0x7f080007;
-        public static final int Location=0x7f080004;
-        public static final int LocationPicker=0x7f080005;
-        public static final int Log=0x7f080008;
-        public static final int Submit=0x7f080009;
-        public static final int Upload=0x7f080006;
-        public static final int action_settings=0x7f08000b;
-        public static final int pager=0x7f080000;
-        public static final int textView=0x7f080003;
+        public static final int Cancel=0x7f09000a;
+        public static final int Category=0x7f090001;
+        public static final int CategoryList=0x7f090002;
+        public static final int ImageVideoPicker=0x7f090007;
+        public static final int Location=0x7f090004;
+        public static final int LocationPicker=0x7f090005;
+        public static final int Log=0x7f090008;
+        public static final int Submit=0x7f090009;
+        public static final int Upload=0x7f090006;
+        public static final int action_settings=0x7f090011;
+        public static final int pager=0x7f090000;
+        public static final int resultsListView=0x7f090010;
+        public static final int searchButton=0x7f09000f;
+        public static final int searchEditText=0x7f09000e;
+        public static final int searchTextView=0x7f09000d;
+        public static final int spinner=0x7f09000c;
+        public static final int textView=0x7f090003;
+        public static final int textView2=0x7f09000b;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -45,12 +54,14 @@ public final class R {
         public static final int windows_frag=0x7f030003;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
+        public static final int main=0x7f080000;
     }
     public static final class string {
         public static final int action_settings=0x7f050002;
         public static final int app_name=0x7f050000;
+        public static final int category_text=0x7f050003;
         public static final int hello_world=0x7f050001;
+        public static final int search_text=0x7f050004;
     }
     public static final class style {
         /** 
@@ -74,10 +85,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
     }
 }
