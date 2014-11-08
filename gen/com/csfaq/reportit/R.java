d0ba714f25acf3e05e689cad39c277e28e56ba09
shruti-verma <shrutiv@amazon.com>
2014-11-08 12:26:04 +0530

    Commitinmg Changes for Complaint tab

--- gen/com/csfaq/reportit/R.java
+++ gen/com/csfaq/reportit/R.java
@@ -10,6 +10,7 @@ package com.csfaq.reportit;
 public final class R {
     public static final class array {
         public static final int complaint_category=0x7f060000;
+        public static final int logas_category=0x7f060001;
     }
     public static final class attr {
     }
@@ -25,27 +26,32 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int back=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int Cancel=0x7f09000a;
-        public static final int Category=0x7f090001;
-        public static final int CategoryList=0x7f090002;
+        public static final int Cancel=0x7f09000b;
         public static final int ImageVideoPicker=0x7f090007;
         public static final int Location=0x7f090004;
         public static final int LocationPicker=0x7f090005;
         public static final int Log=0x7f090008;
-        public static final int Submit=0x7f090009;
+        public static final int Submit=0x7f09000a;
         public static final int Upload=0x7f090006;
-        public static final int action_settings=0x7f090011;
+        public static final int action_settings=0x7f090015;
+        public static final int completed=0x7f09000e;
+        public static final int completedCount=0x7f09000f;
+        public static final int description=0x7f090003;
+        public static final int logspinner=0x7f090009;
         public static final int pager=0x7f090000;
-        public static final int resultsListView=0x7f090010;
-        public static final int searchButton=0x7f09000f;
-        public static final int searchEditText=0x7f09000e;
-        public static final int searchTextView=0x7f09000d;
-        public static final int spinner=0x7f09000c;
-        public static final int textView=0x7f090003;
-        public static final int textView2=0x7f09000b;
+        public static final int resultsListView=0x7f090013;
+        public static final int searchButton=0x7f090012;
+        public static final int searchEditText=0x7f090011;
+        public static final int searchTextView=0x7f090010;
+        public static final int spinner=0x7f090002;
+        public static final int textView=0x7f090014;
+        public static final int textView2=0x7f090001;
+        public static final int total=0x7f09000c;
+        public static final int totalCount=0x7f09000d;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -59,9 +65,19 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f050002;
         public static final int app_name=0x7f050000;
+        public static final int cancel=0x7f05000a;
         public static final int category_text=0x7f050003;
+        public static final int completed=0x7f05000c;
+        public static final int completedCount=0x7f05000d;
+        public static final int dummyLocation=0x7f050008;
         public static final int hello_world=0x7f050001;
+        public static final int location=0x7f050006;
+        public static final int logas=0x7f050007;
         public static final int search_text=0x7f050004;
+        public static final int submit=0x7f050009;
+        public static final int total=0x7f05000b;
+        public static final int totalCount=0x7f05000e;
+        public static final int upload=0x7f050005;
     }
     public static final class style {
         /** 
--- src/com/csfaq/reportit/Complain.java
+++ src/com/csfaq/reportit/Complain.java
@@ -4,18 +4,67 @@ import com.csfaq.reportit.R;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class Complain extends Fragment {
+	
 	@Override
 	    public View onCreateView(LayoutInflater inflater, ViewGroup container,
 	            Bundle savedInstanceState) {
 	 
-	        View android = inflater.inflate(R.layout.android_frag, container, false);
-	        
+	        View android1 = inflater.inflate(R.layout.android_frag, container, false);
+	        final Spinner categorySpinner = (Spinner)android1.findViewById(R.id.spinner);
+            
+	        String[] categoryItems = getResources().getStringArray(R.array.complaint_category);
+            ArrayAdapter<String> aa = new ArrayAdapter<String>(this.getActivity(), android.R.layout.simple_list_item_1, categoryItems);
+
+            categorySpinner.setAdapter(aa);
+
+            categorySpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                @Override
+                public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                    Log.i("GHC", categorySpinner.getSelectedItem().toString() + "  " + categorySpinner.getSelectedItemPosition());
+
+                }
+
+                @Override
+                public void onNothingSelected(AdapterView<?> parent) {
+
+                }
+            });
+
+            
+            EditText tv1 = (EditText) android1.findViewById(R.id.description);
+            tv1.setText("");
+            
+    		
+            
+            Button submitButton = (Button)android1.findViewById(R.id.Submit);
+            submitButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+    			                	Toast.makeText(getActivity(), "Thanks for submitting the complaint! We will get back to you shortly! )",
+                			   Toast.LENGTH_LONG).show();           
+                } });
+          
 	     
-	        return android;
+         final Spinner logasSpinner = (Spinner)android1.findViewById(R.id.logspinner);
+            
+	        String[] logItems = getResources().getStringArray(R.array.logas_category);
+            ArrayAdapter<String> logCat = new ArrayAdapter<String>(this.getActivity(), android.R.layout.simple_list_item_1, logItems);
+    logasSpinner.setAdapter(logCat);
+            
+	        return android1;
 }}
