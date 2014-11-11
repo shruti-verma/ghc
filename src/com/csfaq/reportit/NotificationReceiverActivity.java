33125aaef4c568a6d9ff2be0f6fb4e2d80e80e78
shruti-verma <shrutiv@amazon.com>
2014-11-11 11:17:27 +0530

    Submitting code for geting notifications after compliant is submitted

---
+++ src/com/csfaq/reportit/NotificationReceiverActivity.java
@@ -0,0 +1,14 @@
+package com.csfaq.reportit;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.csfaq.reportit.R;
+
+public class NotificationReceiverActivity extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.result);
+	}
+}
