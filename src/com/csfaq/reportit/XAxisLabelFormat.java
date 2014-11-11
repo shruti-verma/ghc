d6a2335c13dd4813872cf773a128eb399ff59b02
ShaileshVashistha <shaileshsha@gmail.com>
2014-11-11 12:01:21 +0530

    Updated the Dashboard to show a graph for complaints logged by category.

---
+++ src/com/csfaq/reportit/XAxisLabelFormat.java
@@ -0,0 +1,33 @@
+package com.csfaq.reportit;
+
+import android.os.Bundle;
+import java.text.FieldPosition;
+import java.text.Format;
+import java.text.ParsePosition;
+
+/**
+ * Created by shash on 11/11/2014.
+ */
+public class XAxisLabelFormat extends Format {
+        public String[] Labels;
+
+        public XAxisLabelFormat(String[] labels) {
+        Labels = labels;
+        }
+
+        @Override
+        public StringBuffer format(Object object, StringBuffer buffer, FieldPosition field)
+        {
+            int parsedInt =  Math.round(((Number) object).floatValue());
+            String labelString = Labels[parsedInt];
+            buffer.append(labelString);
+            return buffer;
+        }
+
+
+        @Override
+        public Object parseObject(String string, ParsePosition position) {
+            return java.util.Arrays.asList(Labels).indexOf(string);
+        }
+    }
+
